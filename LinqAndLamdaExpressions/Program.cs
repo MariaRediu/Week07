3e3267cc51e96b4d434aa2bada0663873fe4c431
Maria <Maria@MARIA-PC>
2019-11-27 15:19:22 +0200

    update tema

--- LinqAndLamdaExpressions/Program.cs
+++ LinqAndLamdaExpressions/Program.cs
@@ -37,8 +37,12 @@
                }
                */
 
-            // 3 - print number of posts for each user.
 
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 3 - print number of posts for each user.
+            /*
 
                var numberId = allPosts.Select(s => s.Id);
               foreach (var elem in numberId)
@@ -47,30 +51,39 @@
               }
               Console.ReadLine(); 
 
+            */
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 4 numberId lat and long negative.(CORECT)
+            
+            var number = allUsers.Where(x => x.Address.Geo.Lat<0 && x.Address.Geo.Lng<0).ToList();
 
-            // 4 numberId lat and long negative. NU CRED CA E CORECT?????????????????????????????
-
-            var number = allUsers.OrderBy(x => x.Address.Geo.Lat<0).ThenBy(x => x.Address.Geo.Lng<0);
-            Console.WriteLine(number);
+            foreach (var elem in number)
+            {
+                Console.WriteLine($"The ID   {elem.Id} is with  lat {elem.Address.Geo.Lat} and long {elem.Address.Geo.Lng}");
+            }
 
             Console.ReadLine();
-
+            
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             // 5 - find the post with longest body.  
-
+            /*
 
              var longest = allPosts.OrderByDescending(x => x.Body.Length).First();
               Console.WriteLine(longest.Id);
+            */
 
-
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             // 6 - print the name of the employee that have post with longest body.
-
+            /*
           var nume = allUsers.Where(x => x.Name.Equals(longest)).ToList();
             Console.WriteLine(nume);
             Console.ReadLine();
 
-
+            */
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             // 7 - select all addresses in a new List<Address>. print the list. NU CRED CA E CORECT   ??????????????????
+            /*
            List<Address> newAdress = new List<Address>();
              var adress = (from s in newAdress select new { s.Street, s.Suite, s.City, s.Zipcode, s.Geo }).ToList();
              foreach (var item in adress)
@@ -78,47 +91,48 @@
                  Console.WriteLine(item.Suite, item.Suite, item.City, item.Zipcode, item.Geo);
              }
              Console.ReadLine();
-
-
-
-
-            // 8 - print the user with min lat 
+             */
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 8 - print the user with min lat nu stiu cum sa fac print-ul
+            /*
              var minLatitude = allUsers.Min(x => x.Address.Geo.Lat);
              Console.WriteLine($"Minim of latitude is {minLatitude}");
+            */
 
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 9 - print the user with max long  nu stiu cum sa fac print-ul
+            /*
+            var maxLatitude = allUsers.Max(x => x.Address.Geo.Lng);
+             Console.WriteLine($" Maximum of long is {maxLatitude}");
+            
+            */
 
-
-            // 9 - print the user with max long 
-             var maxLatitude = allUsers.Max(x => x.Address.Geo.Lng);
-             Console.WriteLine($"Maximum of long is {maxLatitude}");
-
-         
-
-
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////
             // 10 - create a new class: public class UserPosts { public User User {get; set}; public List<Post> Posts {get; set} }
             //    - create a new list: List<UserPosts>
-            //    - insert in this list each user with his posts only     NU STIU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+            //    - insert in this list each user with his posts only     NU STIU!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!CUM AS PUTEA SA FAC?
+            /*
                     List<UserPosts> userPosts = new List<UserPosts>();
                      UserPosts user1 = new UserPosts();
                   //   userPosts.AddRange();   ????????
                 
+             */
 
 
 
 
-
-
-            // 11 - order users by zip code 
-
-            var UserByZipCode = allUsers.OrderBy(x => x.Address.Zipcode);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 11 - order users by zip code (CORECT)
+            /*
+            var UserByZipCode = allUsers.OrderBy(x => x.Address.Zipcode).ToList();
 
                foreach (var elem in UserByZipCode)
                {
-                   Console.WriteLine(elem.Address.Zipcode);
+                   Console.WriteLine($" USER NAME: {elem.Name}, ZIPCODE:{ elem.Address.Zipcode}");
                }
                Console.ReadLine();  
-
+            */
+            /////////////////////////////////////////////////////////////////////////////////////////////////////
             // 12 - order users by number of posts  ?????????????????????????????????
 
 
